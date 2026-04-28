f0c35de4b4ccf34bcb0a0901cdd26520cd512a3f
dderaad <dideraad@gmail.com>
2026-04-28 14:52:25 -0700

    bug fixes fixed systematic error in seed generation

--- fast_sim.py
+++ fast_sim.py
@@ -6,7 +6,7 @@ import pandas as pd
 import time, datetime
 import os
 
-
+MAX_INT32 = int(2**32)
 PATH = r"data"
 TEMP_PATH = r"data\temp"
 
@@ -24,18 +24,18 @@ def _sim(seed, N):
 
 def par_sim(target=100_000):
     # Save seed for reproducibility
-    seed = (time.time_ns() // 1000) % 10_000
+    seed = (time.time_ns() // 100) % MAX_INT32
 
     # Parallel Parameters:
     n_workers = os.cpu_count()
-    batch_size = 1_000
+    batch_size = 10_000
     chunk_size = 1
     n_jobs = np.max([np.ceil(target / batch_size), 1]).astype(int)  # type: ignore
 
     # parameters for process_map
     rng = np.random.default_rng(seed)
-    seed_list = rng.integers(100, 
-                             10_000, # arbitrary
+    seed_list = rng.integers(1, 
+                             MAX_INT32 - 1,
                              n_jobs)
     batch_sizes = [batch_size] * n_jobs
 
@@ -50,8 +50,15 @@ def par_sim(target=100_000):
     p = results["regular"].sum() / results["N"].sum()
     print(f"{p=}")
     print(f"saving...")
-    results.to_pickle(fr"{PATH}\results_{seed:04}_{time.time_ns()}.pkl")
+    results.to_pickle(fr"{PATH}\results_{seed:010}.pkl")
     
 
 if __name__ == "__main__":
-    par_sim(50_000_000)
+    j = 0
+    while True:
+        m = 50
+        for i in range(m):
+            par_sim(1_000_000)
+        
+        j += 1
+        f"generating data for {j} epochs."
---
+++ proc.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import sys, os
+from statsmodels.stats.proportion import proportion_confint
+from scipy.stats import binomtest
+
+PATH = r"data"
+
+def process_data():
+    used_seeds = set()
+    datasets = []
+    for i, f in enumerate(os.listdir(rf"{PATH}/")):
+        if f == ".dataignore":
+            continue
+        r = pd.read_pickle(rf"{PATH}/{f}")
+        datasets.append(r)
+
+    mass = pd.concat(datasets)
+    
+    viliam_data = (1139883093, 3_000_000_000)
+    #viliam_data = (0, 0)
+
+    successes, trials = np.sum(mass["regular"]) + viliam_data[0], np.sum(mass["N"]) + viliam_data[1]
+    p = successes / trials
+    print(f"{successes=:,}\n{trials=:,}")
+
+    alpha = 0.05
+    print(proportion_confint(successes, trials, alpha=alpha, method='beta'))
+    print(binomtest(successes, trials).proportion_ci(confidence_level=1-alpha))
+
+    print(f"{p=}")
+
+    return mass
+    
+
+if __name__ == "__main__":
+    process_data()
